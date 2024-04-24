e047d0a32c73c70d753af7fa5201eac0ad5c586f
Ilfat Arslanov <123643044+wise-air@users.noreply.github.com>
2024-04-24 22:52:12 +0500

    Update urls.py

--- cookingbook/cookingbook/urls.py
+++ cookingbook/cookingbook/urls.py
@@ -23,8 +23,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RecipeView.as_view(), name='recipe_api'),
-    path('main/', include('recipes.urls')),
+    path('api', RecipeView.as_view(), name='recipe_api'),
+    path('', include('recipes.urls')),
     path('register/', user_views.register, name="user-register"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="user-logout"),
